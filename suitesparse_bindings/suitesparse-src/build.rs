cc23c9f8ea1d9d47e26c37972ea1d7ef74b9767a
James Logan <jlogan03@gmail.com>
2023-06-08 16:22:56 -0400

    get amd to build both DI and DL versions in one archive. WIP to get cholmod and umfpack to build.

--- suitesparse_bindings/suitesparse-src/build.rs
+++ suitesparse_bindings/suitesparse-src/build.rs
@@ -1,3 +1,5 @@
+use std::path::{PathBuf, Path};
+
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     let root = std::env::var_os("OUT_DIR").unwrap();
@@ -67,6 +69,96 @@ fn main() {
             .cargo_metadata(false)
             .compile("ldl");
     }
+    if cfg!(feature = "umfpack") {
+        suitesparse_config = true;
+
+        let amd_files = get_source_files(PathBuf::from("SuiteSparse/AMD/Source"));
+
+        let mut cholmod_files = get_source_files(PathBuf::from("SuiteSparse/CHOLMOD/Cholesky"));
+        cholmod_files.append(&mut get_source_files(PathBuf::from("SuiteSparse/CHOLMOD/Partition")));
+        cholmod_files.append(&mut get_source_files(PathBuf::from("SuiteSparse/CHOLMOD/Modify")));
+        cholmod_files.append(&mut get_source_files(PathBuf::from("SuiteSparse/CHOLMOD/Supernodal")));
+        cholmod_files.append(&mut get_source_files(PathBuf::from("SuiteSparse/CHOLMOD/GPU")));
+        cholmod_files.append(&mut get_source_files(PathBuf::from("SuiteSparse/CHOLMOD/MatrixOps")));
+
+        let umfpack_files = get_source_files(PathBuf::from("SuiteSparse/UMFPACK/Source"));
+
+        // Build AMD
+        //    Double-Int version
+        cc::Build::new()
+            .include("SuiteSparse/SuiteSparse_config")
+            .include("SuiteSparse/AMD/Include")
+            .files(&amd_files)
+            // .define("DINT", None)
+            .compile("amd");
+
+        let objs = get_files_of_kind(PathBuf::from(&root).join("SuiteSparse/AMD/Source"), "o");
+        println!("{objs:?}");
+        add_object_file_prefixes(PathBuf::from(&root).join("SuiteSparse/AMD/Source").to_str().unwrap(), "di_");
+
+        //    Double-Long version & combine with double-int
+        cc::Build::new()
+            .include("SuiteSparse/SuiteSparse_config")
+            .include("SuiteSparse/AMD/Include")
+            .files(&amd_files)
+            .define("DLONG", None)
+            .object(PathBuf::from(&root).join("SuiteSparse/AMD/Source/di_amd_1.o"))
+            .object(PathBuf::from(&root).join("SuiteSparse/AMD/Source/di_amd_2.o"))
+            .object(PathBuf::from(&root).join("SuiteSparse/AMD/Source/di_amd_aat.o"))
+            .object(PathBuf::from(&root).join("SuiteSparse/AMD/Source/di_amd_control.o"))
+            .object(PathBuf::from(&root).join("SuiteSparse/AMD/Source/di_amd_defaults.o"))
+            .object(PathBuf::from(&root).join("SuiteSparse/AMD/Source/di_amd_dump.o"))
+            .object(PathBuf::from(&root).join("SuiteSparse/AMD/Source/di_amd_global.o"))
+            .object(PathBuf::from(&root).join("SuiteSparse/AMD/Source/di_amd_info.o"))
+            .object(PathBuf::from(&root).join("SuiteSparse/AMD/Source/di_amd_order.o"))
+            .object(PathBuf::from(&root).join("SuiteSparse/AMD/Source/di_amd_post_tree.o"))
+            .object(PathBuf::from(&root).join("SuiteSparse/AMD/Source/di_amd_postorder.o"))
+            .object(PathBuf::from(&root).join("SuiteSparse/AMD/Source/di_amd_preprocess.o"))
+            .object(PathBuf::from(&root).join("SuiteSparse/AMD/Source/di_amd_valid.o"))
+            .compile("amd");
+
+        // Build CHOLMOD
+        //    Double-Int version
+        cc::Build::new()
+            .include("SuiteSparse/SuiteSparse_config")
+            .include("SuiteSparse/CHOLMOD/Include")
+            .files(&cholmod_files)
+            .compile("cholmodi");
+        // copy_file("SuiteSparse/CHOLMOD/Source/cholmod.o", "cholmod_di.o");
+
+        //    Double-Long version & combine with double-int
+        cc::Build::new()
+            .include("SuiteSparse/SuiteSparse_config")
+            .include("SuiteSparse/CHOLMOD/Include")
+            .files(&cholmod_files)
+            .define("DLONG", None)
+            .object("SuiteSparse/CHOLMOD/Source/cholmod_di.o")
+            .compile("cholmod");
+
+        // Build UMFPACK
+        //    Double-Int version
+        cc::Build::new()
+            .include("SuiteSparse/SuiteSparse_config")
+            .include("SuiteSparse/UMFPACK/Include")
+            .files(&umfpack_files)
+            .object("SuiteSparse/AMD/Source/amd.o")
+            .object("SuiteSparse/CHOLMOD/Source/cholmod.o")
+            .define("DINT", None)
+            .compile("umfpacki");
+        // copy_file("SuiteSparse/UMFPACK/Source/umfpack.o", "umfpack_di.o");
+
+        //    Double-Long version & combine with double-int
+        cc::Build::new()
+            .include("SuiteSparse/SuiteSparse_config")
+            .include("SuiteSparse/UMFPACK/Include")
+            .files(&umfpack_files)
+            .object("SuiteSparse/AMD/Source/amd.o")
+            .object("SuiteSparse/CHOLMOD/Source/cholmod.o")
+            .object("SuiteSparse/UMFPACK/Source/umfpack_di.o")
+            .define("DLONG", None)
+            .compile("umfpack");
+
+    }
     if suitesparse_config {
         cc::Build::new()
             .include("SuiteSparse/SuiteSparse_config")
@@ -75,4 +167,45 @@ fn main() {
             .compile("suitesparseconfig");
     }
     println!("cargo:root={}", root.to_string_lossy());
+}
+
+/// Returns a vector of all the .c files in a given directory
+fn get_files_of_kind(dir: PathBuf, kind: &str) -> Vec<PathBuf> {
+    let mut sources = Vec::new();
+    for entry in dir.read_dir().unwrap() {
+        if let Ok(entry) = entry {
+            let path = entry.path();
+            if path.extension().and_then(|s| s.to_str()) == Some(kind) {
+                sources.push(path);
+            }
+        }
+    }
+    sources
+}
+
+fn get_source_files(dir: PathBuf) -> Vec<PathBuf> {
+    return get_files_of_kind(dir, "c")
+}
+
+fn get_object_files(dir: PathBuf) -> Vec<PathBuf> {
+    return get_files_of_kind(dir, "o")
+}
+
+/// Copy object files in a directory, adding prefixes
+/// then return the paths to the new files
+fn add_object_file_prefixes(dir: &str, prefix: &str) -> Vec<PathBuf> {
+    let dir = PathBuf::from(dir);
+    let objects = get_object_files(dir);
+    let mut new_paths: Vec<PathBuf> = Vec::new();
+    for object in objects {
+        let name = object.file_name().unwrap().to_str().unwrap();
+        let new_name = format!("{prefix}{name}");
+        let mut newpath = object.clone();
+        newpath.set_file_name(new_name);
+        println!("cargo:warn={object:?},{newpath:?}");
+        std::fs::copy(&object, &newpath).unwrap();
+        new_paths.push(newpath);
+    }
+    // println!("cargo:warn={new_paths:?}");
+    new_paths
 }
--- suitesparse_bindings/suitesparse_umfpack_sys/build.rs
+++ suitesparse_bindings/suitesparse_umfpack_sys/build.rs
@@ -1,4 +1,15 @@
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rustc-link-lib=umfpack");
+    if !cfg!(feature = "static") {
+        // Dynamically link
+        println!("cargo:rustc-link-lib=umfpack");
+    }
+    else {
+        // Add the built libraries to path
+        let path_to_src = std::env::var("DEP_SUITESPARSE_SRC_ROOT").unwrap();
+        println!("cargo:rustc-link-search=native={path_to_src}");
+        // Statically link umfpack
+        println!("cargo:rustc-link-lib=static=umfpack");
+    }
+
 }
